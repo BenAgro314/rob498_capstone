9b4f30fe9c55437977164b6c20729dddb098ae32
Ben Agro <ben.agro@mail.utoronto.ca>
2023-02-12 21:04:31 -0500

    new workspace

--- README.md
+++ README.md
@@ -7,4 +7,6 @@
 - [ROB498 Docker image](https://hub.docker.com/r/utrarobosoccer/rob498)
 - [Tutorial For Setting up PX4 <-> Nano](https://www.youtube.com/watch?v=Brkk0ZmnGgs)
 - [PX4 VIO docs](https://docs.px4.io/main/en/computer_vision/visual_inertial_odometry.html)
+- [Wifi Debugging](https://forums.developer.nvidia.com/t/jetson-nano-wifi/72269/21)
+	- Maybe this command worked: sudo iw dev wlan0 set power_save off
 
---
+++ build/ROB498/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rob498/catkin_ws/devel/.private/ROB498:$CMAKE_PREFIX_PATH"
+export PWD='/home/rob498/catkin_ws/build/ROB498'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rob498/catkin_ws/devel/.private/ROB498/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/rob498/catkin_ws/src/ROB498/drone:$ROS_PACKAGE_PATH"
