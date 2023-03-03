e688594a30ba2cc251272396247c2c0f75111147
Ben Agro <ben.agro@mail.utoronto.ca>
2023-03-03 12:40:42 -0500

    fix queue logic

--- src/offboard_py/scripts/rob498_drone.py
+++ src/offboard_py/scripts/rob498_drone.py
@@ -17,11 +17,11 @@ class RobDroneControl():
         self.srv_abort = rospy.Service('comm/abort', Empty, self.abort_cb)
 
         # TODO: make these arguments / config files
-        self.waypoint_ths = 0.025 # used in pose_is_close
+        self.waypoint_ths = 0.2 # used in pose_is_close
         self.on_ground_ths = 0.2
         self.launch_height = 1.5 # check this
-        self.num_launch_waypoints = 10
-        self.num_land_waypoints = 10
+        self.num_launch_waypoints = 3
+        self.num_land_waypoints = 3
         self.land_height = 0.15
 
         self.state_sub = rospy.Subscriber("mavros/state", State, callback = self.state_cb)
@@ -49,9 +49,10 @@ class RobDroneControl():
             print(f"Cannot launch, not on ground, or waypoint queue is not empty: {self.waypoint_queue}")
             return EmptyResponse()
 
+        print("Launching")
         for i in range(self.num_launch_waypoints):
             pose = deepcopy(self.current_pose)
-            pose.pose.point.z = self.launch_height * (float(i+1) / float(self.num_launch_waypoints))
+            pose.pose.position.z = self.launch_height * (float(i+1) / float(self.num_launch_waypoints))
             self.waypoint_queue.append(pose)
         return EmptyResponse()
 
@@ -59,20 +60,21 @@ class RobDroneControl():
         if not self.active:
             print("Cannot land, not connected yet")
             return EmptyResponse()
-        if len(self.waypoint_queue) == 0:
+        if len(self.waypoint_queue) != 0:
             print("Cannot land, there are waypoints on the queue")
             return EmptyResponse()
 
-        current_height = self.current_pose.pose.point.z
-
+        print("Landing")
+        current_height = self.current_pose.pose.position.z
         for i in range(self.num_land_waypoints):
             pose = deepcopy(self.current_pose)
-            pose.pose.point.z = current_height * (1.0 - (float(i+1) / float(self.num_launch_waypoints))) + self.land_height
+            pose.pose.position.z = current_height * (1.0 - (float(i+1) / float(self.num_land_waypoints))) + self.land_height
             self.waypoint_queue.append(pose)
         return EmptyResponse()
 
     def abort_cb(self, request: Empty):
         self.current_waypoint = self.current_pose
+        print("Aborting!")
         self.waypoint_queue = []
         return EmptyResponse()
 
@@ -80,13 +82,13 @@ class RobDroneControl():
         return EmptyResponse()
 
     def can_launch(self):
-        return self.current_pose.pose.point.z < self.on_ground_ths and len(self.waypoint_queue) == 0
+        return self.current_pose.pose.position.z < self.on_ground_ths and len(self.waypoint_queue) == 0
 
     def pose_is_close(self, pose1: PoseStamped, pose2: PoseStamped):
         # TOOD: include some meaasure of rotation diff
-        pt1 = pose1.pose.point
+        pt1 = pose1.pose.position
         pt1 = np.array([pt1.x, pt1.y, pt1.z])
-        pt2 = pose2.pose.point
+        pt2 = pose2.pose.position
         pt2 = np.array([pt2.x, pt2.y, pt2.z])
         return np.linalg.norm(pt1 - pt2) < self.waypoint_ths
 
@@ -94,7 +96,7 @@ class RobDroneControl():
         rate = rospy.Rate(20)
 
         print(f"Trying to connect to drone...")
-        while(not rospy.is_shutdown() and not self.current_state.connected and self.current_pose is None):
+        while((not rospy.is_shutdown() and not self.current_state.connected) or (self.current_pose is None)):
             rate.sleep()
         print(f"Connected!")
         self.active = True
@@ -103,7 +105,7 @@ class RobDroneControl():
             self.setpoint_pub.publish(self.current_waypoint)
             if self.pose_is_close(self.current_waypoint, self.current_pose):
                 if len(self.waypoint_queue) > 0:
-                    self.current_waypoint = self.waypoint_queue.pop(-1)
+                    self.current_waypoint = self.waypoint_queue.pop(0)
             rate.sleep()
 
 if __name__ == "__main__":
