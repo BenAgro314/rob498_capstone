61ae661a3cfc5769deaadabd74db9f3623184b74
Ben Agro <ben.agro@mail.utoronto.ca>
2023-03-03 14:07:39 -0500

    stuff

---
+++ export_connect_to_bens.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+export GROUNDSTATION_IP=100.66.93.211
+export ROS_MASTER_URI=http://localhost:11311
+unset ROS_IP
+
--- export_connect_to_host.sh
+++ export_connect_to_host.sh
@@ -2,3 +2,4 @@
 
 export ROS_IP=10.42.0.101
 export ROS_MASTER_URI=http://10.42.0.100:11311
+export GROUNDSTATION_IP=10.42.0.100
