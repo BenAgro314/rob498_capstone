1d869ec8262e1a06bdf0a66cff9830a648e3e521
Ben Agro <ben.agro@mail.utoronto.ca>
2023-03-09 20:37:02 -0500

    thread safe

--- src/offboard_py/scripts/rob498_drone.py
+++ src/offboard_py/scripts/rob498_drone.py
@@ -3,7 +3,7 @@ from typing import List
 
 from offboard_py.scripts.path_planner import find_traj
 from offboard_py.scripts.utils import Colors
-from queue import Queue
+from threading import Semaphore, Lock
 import rospy
 from copy import deepcopy
 import numpy as np
@@ -11,7 +11,6 @@ from geometry_msgs.msg import PoseStamped, Twist
 from mavros_msgs.msg import State
 from std_srvs.srv import Empty, EmptyResponse
 from nav_msgs.msg import Path
-#import threading
 
 class RobDroneControl():
 
@@ -49,10 +48,33 @@ class RobDroneControl():
 
         self.current_state = State()
 
-        self.waypoint_queue = Queue()
+        self.waypoint_queue = []
         self.current_waypoint = PoseStamped()
         self.active = False
 
+        self.waypoint_queue_num = Semaphore(0)
+        self.waypoint_queue_lock = Lock()
+        self.len_waypoint_queue = 0
+
+
+    def waypoint_queue_push(self, pose: PoseStamped):
+        self.waypoint_queue_lock.acquire()
+
+        self.waypoint_queue.append(pose)
+
+        self.len_waypoint_queue += 1
+        self.waypoint_queue_lock.release()
+        self.waypoint_queue_num.release()
+
+    def waypoint_queue_pop(self):
+        self.waypoint_queue_num.acquire()
+        self.waypoint_queue_lock.acquire()
+
+        res = self.waypoint_queue.pop(0)
+
+        self.len_waypoint_queue -= 1
+        self.waypoint_queue_lock.release()
+        return res
 
     def pose_cb(self, pose: PoseStamped):
         self.current_pose = pose
@@ -61,15 +83,10 @@ class RobDroneControl():
         self.current_state = msg
 
     def publish_current_queue(self):
-        pass
-        #q_copy = deepcopy(self.waypoint_queue)
-        #msg = Path()
-        #msg.header.frame_id = self.current_pose.header.frame_id
-        #msg.poses = [self.current_pose] 
-        #for _ in range(q_copy.qsize()):
-        #    p = q_copy.get_nowait()
-        #    msg.poses.append(p)
-        #self.current_path_pub.publish(msg)
+        msg = Path()
+        msg.header.frame_id = self.current_pose.header.frame_id
+        msg.poses = [self.current_pose] + self.waypoint_queue
+        self.current_path_pub.publish(msg)
 
     def launch_cb(self, request: Empty):
         #self.queue_lock.acquire()
@@ -78,14 +95,14 @@ class RobDroneControl():
             print("Cannot launch, not connected yet")
             return EmptyResponse()
         if not self.can_launch():
-            print(f"Cannot launch, not on ground, or waypoint queue is not empty")
+            print(f"Cannot launch, not on ground, or waypoint queue is not empty: {self.len_waypoint_queue}")
             return EmptyResponse()
 
         print(f"{Colors.GREEN}LAUNCHING{Colors.RESET}")
         for i in range(self.num_launch_waypoints):
             pose = deepcopy(self.current_pose)
             pose.pose.position.z = self.launch_height * (float(i+1) / float(self.num_launch_waypoints))
-            self.waypoint_queue.put(pose)
+            self.waypoint_queue_push(pose)
 
         self.publish_current_queue()
 
@@ -97,7 +114,7 @@ class RobDroneControl():
         if not self.active:
             print("Cannot land, not connected yet")
             return EmptyResponse()
-        if not self.waypoint_queue.empty(): 
+        if self.len_waypoint_queue != 0: # TODO: dangerous
             print("Cannot land, there are waypoints on the queue")
             return EmptyResponse()
 
@@ -106,7 +123,7 @@ class RobDroneControl():
         for i in range(self.num_land_waypoints):
             pose = deepcopy(self.current_pose)
             pose.pose.position.z = current_height * (1.0 - (float(i+1) / float(self.num_land_waypoints))) + self.land_height
-            self.waypoint_queue.put(pose)
+            self.waypoint_queue_push(pose)
 
         self.publish_current_queue()
         #self.queue_lock.release()
@@ -114,11 +131,7 @@ class RobDroneControl():
 
     def abort_cb(self, request: Empty):
         print(f"{Colors.RED}ABORTING{Colors.RESET}")
-        self.current_waypoint = self.current_pose
-        while self.waypoint_queue.qsize() > 0:
-            self.waypoint_queue.get_nowait()
-
-        self.publish_current_queue()
+        exit()
         return EmptyResponse()
 
     def test_task3(self, points: np.array):
@@ -126,6 +139,7 @@ class RobDroneControl():
         obstacle_radius = 0.3
         start = np.array([self.current_pose.pose.position.x, self.current_pose.pose.position.y, self.current_pose.pose.position.z])[None, :]
         points = np.concatenate((start, points), axis = 0)
+        trajs = []
         for i in range(len(points) - 1):
             start_ind = i
             goal_ind = i + 1
@@ -139,16 +153,24 @@ class RobDroneControl():
             )
             if traj is None:
                 print(f"{Colors.RED}FAILED TO FIND PATH, STOPPING{Colors.RESET}")
-                while self.waypoint_queue.qsize() > 0:
-                    self.waypoint_queue.get_nowait()
                 return
-            for p in traj[1:]: # exclude current pose from traj
-                m = PoseStamped()
-                m.header.frame_id = self.current_pose.header.frame_id
-                m.pose.position.x = p[0]
-                m.pose.position.y = p[1]
-                m.pose.position.z = p[2]
-                self.waypoint_queue.put(m)
+            trajs.append(traj[1:])
+
+        trajs = np.concatenate(trajs, axis = 0)
+        self.waypoint_queue_lock.acquire()
+        for p in trajs:
+            m = PoseStamped()
+            m.header.frame_id = self.current_pose.header.frame_id
+            m.pose.position.x = p[0]
+            m.pose.position.y = p[1]
+            m.pose.position.z = p[2]
+            self.waypoint_queue.append(m)
+            #self.waypoint_queue_push(m)
+            self.waypoint_queue_num.release()
+            self.len_waypoint_queue += 1
+
+        self.publish_current_queue()
+        self.waypoint_queue_lock.release()
 
     def test_cb(self, request: Empty):
         #self.queue_lock.acquire()
@@ -157,7 +179,7 @@ class RobDroneControl():
             print("Cannot test, not connected yet")
             return EmptyResponse()
         if not self.can_test():
-            print(f"Cannot test, not launched or waypoint queue is not empty")
+            print(f"Cannot test, not launched or waypoint queue is not empty : {self.len_waypoint_queue}")
             return EmptyResponse()
         points = np.random.rand(6, 3) * 2 + 1
         self.test_task3(points)
@@ -167,10 +189,10 @@ class RobDroneControl():
         return EmptyResponse()
 
     def can_launch(self):
-        return self.current_pose.pose.position.z < self.on_ground_ths and self.waypoint_queue.empty()
+        return self.current_pose.pose.position.z < self.on_ground_ths and self.len_waypoint_queue == 0
 
     def can_test(self):
-        return self.waypoint_queue.empty()
+        return self.len_waypoint_queue == 0
 
     def pose_is_close(self, pose1: PoseStamped, pose2: PoseStamped):
         # TOOD: include some meaasure of rotation diff
@@ -212,8 +234,8 @@ class RobDroneControl():
             self.setpoint_vel_pub.publish(self.compute_twist_command())
             if self.pose_is_close(self.current_waypoint, self.current_pose):
                 #self.queue_lock.acquire()
-                if not self.waypoint_queue.empty():
-                    self.current_waypoint = self.waypoint_queue.get()
+                if self.len_waypoint_queue > 0:
+                    self.current_waypoint = self.waypoint_queue_pop()
                     self.current_waypoint_pub.publish(self.current_waypoint)
                 #self.queue_lock.release()
             rate.sleep()
