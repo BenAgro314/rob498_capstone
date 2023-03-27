c3343eca6be390880e7ea6b303e0e494cb21dc4b
Ben Agro <ben.agro@mail.utoronto.ca>
2023-03-27 09:58:26 -0400

    frame names

--- src/offboard_py/scripts/fake_vicon_data.py
+++ src/offboard_py/scripts/fake_vicon_data.py
@@ -13,7 +13,12 @@ class FakeVicon:
 		self.link_pose.transform.translation.x = 10.0
 		self.link_pose.transform.translation.y = 10.0
 		self.link_pose.transform.translation.z = 10.0
+		self.link_pose.transform.rotation.x = 0.0
+		self.link_pose.transform.rotation.y = 0.0
+		self.link_pose.transform.rotation.z = 0.0
 		self.link_pose.transform.rotation.w = 1.0
+		self.link_pose.header.frame_id="camera_odom_frame"
+		self.link_pose.child_frame_id="camera_pose_frame"
 
 	#def update(self):
 		#self.link_pose.transform.translation.z = 0.1 * (rospy.Time.now().to_sec() - self.start_time.to_sec())
@@ -22,9 +27,7 @@ class FakeVicon:
 if __name__ == '__main__':
 	rospy.init_node('fake_vicon', anonymous=True)
 	fv = FakeVicon()
-	publish_rate = rospy.get_param('~publish_rate', 10)
-
-	rate = rospy.Rate(publish_rate)
+	rate = rospy.Rate(20)
 	while not rospy.is_shutdown():
 		fv.pose_pub.publish(fv.link_pose)
 		#fv.update()
