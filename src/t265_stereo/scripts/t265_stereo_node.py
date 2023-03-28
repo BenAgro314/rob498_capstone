1481c5fc5d0bf0ddcb5134d6bb51eedfa73862bf
Cameron <cwitkowski@rogers.com>
2023-03-28 14:42:09 -0400

    rect

--- src/t265_stereo/scripts/t265_stereo_node.py
+++ src/t265_stereo/scripts/t265_stereo_node.py
@@ -4,6 +4,8 @@
 import rospy
 import cv2
 import numpy as np
+import pytesseract
+from imutils.object_detection import non_max_suppression
 from math import tan, pi
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -20,6 +22,56 @@ parameter distortion model known as Kanalla-Brandt. OpenCV supports this
 distortion model in their "fisheye" module, more details can be found here:
 https://docs.opencv.org/3.4/db/d58/group__calib3d__fisheye.html
 """
+
+
+def decode_predictions(scores, geometry):
+	# grab the number of rows and columns from the scores volume, then
+	# initialize our set of bounding box rectangles and corresponding
+	# confidence scores
+	(numRows, numCols) = scores.shape[2:4]
+	rects = []
+	confidences = []
+	# loop over the number of rows
+	for y in range(0, numRows):
+		# extract the scores (probabilities), followed by the
+		# geometrical data used to derive potential bounding box
+		# coordinates that surround text
+		scoresData = scores[0, 0, y]
+		xData0 = geometry[0, 0, y]
+		xData1 = geometry[0, 1, y]
+		xData2 = geometry[0, 2, y]
+		xData3 = geometry[0, 3, y]
+		anglesData = geometry[0, 4, y]
+		# loop over the number of columns
+		for x in range(0, numCols):
+			# if our score does not have sufficient probability,
+			# ignore it
+			if scoresData[x] < 0.9:
+				continue
+			# compute the offset factor as our resulting feature
+			# maps will be 4x smaller than the input image
+			(offsetX, offsetY) = (x * 4.0, y * 4.0)
+			# extract the rotation angle for the prediction and
+			# then compute the sin and cosine
+			angle = anglesData[x]
+			cos = np.cos(angle)
+			sin = np.sin(angle)
+			# use the geometry volume to derive the width and height
+			# of the bounding box
+			h = xData0[x] + xData2[x]
+			w = xData1[x] + xData3[x]
+			# compute both the starting and ending (x, y)-coordinates
+			# for the text prediction bounding box
+			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+			startX = int(endX - w)
+			startY = int(endY - h)
+			# add the bounding box coordinates and probability score
+			# to our respective lists
+			rects.append((startX, startY, endX, endY))
+			confidences.append(scoresData[x])
+	# return a tuple of the bounding boxes and associated confidences
+	return (rects, confidences)
 
 """
 Returns R, T transform from src to dst
@@ -123,6 +175,7 @@ try:
     D_right = fisheye_distortion(intrinsics["right"])
     (width, height) = (intrinsics["left"].width, intrinsics["left"].height)
 
+
     # Get the relative extrinsics between the left and right camera
     (R, T) = get_extrinsics(streams["left"], streams["right"])
 
@@ -216,6 +269,51 @@ try:
             disp_vis = 255*(disparity - min_disp)/ num_disp
             disp_color = cv2.applyColorMap(cv2.convertScaleAbs(disp_vis,1), cv2.COLORMAP_JET)
             color_image = cv2.cvtColor(center_undistorted["left"][:,max_disp:], cv2.COLOR_GRAY2RGB)
+            origH, origW, Channels = color_image.shape
+
+            print(origH)
+            print(origW)
+            quit()
+            layerNames = [
+            "feature_fusion/Conv_7/Sigmoid",
+            "feature_fusion/concat_3"]
+            # load the pre-trained EAST text detector
+            print("[INFO] loading EAST text detector...")
+            net = cv2.dnn.readNet("frozen_east_text_detection.pb")
+
+            # construct a blob from the image and then perform a forward pass of
+            # the model to obtain the two output layer sets
+            blob = cv2.dnn.blobFromImage(color_image, 1.0, (W, H),
+                (123.68, 116.78, 103.94), swapRB=True, crop=False)
+            net.setInput(blob)
+            (scores, geometry) = net.forward(layerNames)
+            # decode the predictions, then  apply non-maxima suppression to
+            # suppress weak, overlapping bounding boxes
+            (rects, confidences) = decode_predictions(scores, geometry)
+            boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+            # initialize the list of results
+            results = []
+            # loop over the bounding boxes
+            for (startX, startY, endX, endY) in boxes:
+                # scale the bounding box coordinates based on the respective
+                # ratios
+                startX = int(startX * rW)
+                startY = int(startY * rH)
+                endX = int(endX * rW)
+                endY = int(endY * rH)
+                # in order to obtain a better OCR of the text we can potentially
+                # apply a bit of padding surrounding the bounding box -- here we
+                # are computing the deltas in both the x and y directions
+                dX = int((endX - startX) * 0.05)
+                dY = int((endY - startY) * 0.05)
+                # apply padding to each side of the bounding box, respectively
+                startX = max(0, startX - dX)
+                startY = max(0, startY - dY)
+                endX = min(origW, endX + (dX * 2))
+                endY = min(origH, endY + (dY * 2))
+                # extract the actual padded ROI
+                roi = orig[startY:endY, startX:endX]
 
             if mode == "stack":
                 cv2.imshow(WINDOW_TITLE, np.hstack((color_image, disp_color)))
