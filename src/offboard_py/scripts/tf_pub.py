ba7f6a8e4852e44c608a8e9a4013b3b1035e6852
Ben Agro <ben.agro@mail.utoronto.ca>
2023-04-03 10:04:34 -0400

    chmod

---
+++ src/offboard_py/scripts/tf_pub.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+from geometry_msgs.msg import PoseStamped
+
+def pose_callback(pose_msg):
+    # Extract position and orientation from the pose message
+    position = pose_msg.pose.position
+    orientation = pose_msg.pose.orientation
+
+    # Create a transform broadcaster
+    br = tf.TransformBroadcaster()
+
+    # Broadcast the transform
+    br.sendTransform(
+        (position.x, position.y, position.z),
+        (orientation.x, orientation.y, orientation.z, orientation.w),
+        rospy.Time.now(),
+        "base_link",
+        "map"
+    )
+
+def main():
+    # Initialize the ROS node
+    rospy.init_node('pose_to_tf', anonymous=True)
+
+    # Subscribe to the mavros/local_position/pose topic
+    rospy.Subscriber('mavros/local_position/pose', PoseStamped, pose_callback)
+
+    # Keep the node running
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
