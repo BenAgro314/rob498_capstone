24b332ac239ba397f8c14a92e702f02157ff2d0f
Ben Agro <ben.agro@mail.utoronto.ca>
2023-04-06 19:04:13 -0400

    ct4 test

--- scripts/test_task_3.sh
+++ scripts/test_task_3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rostopic pub rob498_drone_01/comm/waypoints geometry_msgs/PoseArray -f src/offboard_py/config/pose_list.yaml
+rostopic pub rob498_drone_01/comm/waypoints geometry_msgs/PoseArray -f src/offboard_py/config/pose_list_ct4.yaml
