76c99c46c3ffba7a52ffbbc437f2ed807fbbd7cf
Ben Agro <ben.agro@mail.utoronto.ca>
2023-04-10 09:50:10 -0400

    calib script

---
+++ scripts/collect_calib_script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import rospy
+import time
+import cv2
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from jetcam.csi_camera import CSICamera
+
+if __name__ == '__main__':
+    # Set the capture resolution (modify as needed)
+    capture_width = 540
+    capture_height = 540
+
+    fps = 30
+    # Initialize the CSICamera from the jetcam package
+    camera = CSICamera(width=capture_width, height=capture_height, capture_fps=fps)
+
+    # Capture images continuously
+    for _ in range(20):
+        # Capture an image from the camera
+        cv_image = camera.read()
+        cv2.imshow('Image', cv_image)
+        cv2.waitKey(0)
+
