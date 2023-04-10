b46e8b6ac854c0f72d67ee69881b9e29b655c4f7
Ben Agro <ben.agro@mail.utoronto.ca>
2023-04-10 11:44:58 -0400

    launch

--- src/offboard_py/scripts/rob498_drone_v2.py
+++ src/offboard_py/scripts/rob498_drone_v2.py
@@ -51,7 +51,7 @@ class RobDroneControl():
         self.waypoint_trans_ths = 0.08 # 0.08 # used in pose_is_close
         self.waypoint_yaw_ths = np.deg2rad(10.0) # used in pose_is_close
         self.on_ground_ths = 0.05
-        self.launch_height = 1.0
+        self.launch_height = 1.5
         self.land_height = 0
         self.task_ball_radius = 0.15
 
