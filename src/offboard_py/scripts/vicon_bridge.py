e0641b99fb5efac7fbf207c9a5a202447a79f451
Ben Agro <ben.agro@mail.utoronto.ca>
2023-04-14 09:31:21 -0400

    vicon

--- src/offboard_py/scripts/vicon_bridge.py
+++ src/offboard_py/scripts/vicon_bridge.py
@@ -11,7 +11,10 @@ class ViconBridge():
     def __init__(self):
         self.mavros_vision_pose_pub = rospy.Publisher("mavros/odometry/out", Odometry, queue_size=10)
         #local_pose_sub = rospy.Subscriber("mavros/local_position/pose", PoseStamped, callback=print_pose)
+        self.last_vicon_update = None
         self.vicon_pose_sub = rospy.Subscriber("vicon/ROB498_Drone/ROB498_Drone", TransformStamped, callback = self.vicon_update)
+        self.realsense_pose_sub = rospy.Subscriber("camera/odom/sample_throttled", Odometry, callback = self.realsense_update) # fail safe
+        self.min_vicon_rate = 40.0
 
     def vicon_update(self, transform: TransformStamped):
         odom = transform_stamped_to_odometry(transform)
@@ -19,6 +22,25 @@ class ViconBridge():
         odom.child_frame_id = "camera_pose_frame"
         odom.header.stamp=rospy.Time.now()
         self.mavros_vision_pose_pub.publish(odom)
+        self.last_vicon_update = rospy.Time.now()
+
+    def realsense_update(self, odom: Odometry):
+        if self.last_vicon_update is None or (rospy.Time.now().to_sec() - self.last_vicon_update.to_sec()) > 1.0/self.min_vicon_rate:
+            odom.header.frame_id = "camera_odom_frame"
+            odom.child_frame_id = "camera_pose_frame"
+            odom.pose.covariance = [0] * 36
+            # Set linear and angular velocities to zero (unknown)
+            odom.twist.twist.linear.x = 0
+            odom.twist.twist.linear.y = 0
+            odom.twist.twist.linear.z = 0
+            odom.twist.twist.angular.x = 0
+            odom.twist.twist.angular.y = 0
+            odom.twist.twist.angular.z = 0
+            # Set twist covariance to zero (unknown)
+            odom.twist.covariance = [0] * 36
+            odom.header.stamp=rospy.Time.now()
+            self.mavros_vision_pose_pub.publish(odom)
+            self.last_vicon_update = rospy.Time.now()
 
 if __name__ == "__main__":
     rospy.init_node("vicon_bridge")
